07b5502837aab1999833e3ab32e98324ae4ab38b
Pablo Cordero <pablocorderoromero@gmail.com>
2025-11-07 23:33:47 +0100

    chore: remove dead .env files

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-    readonly MINIFLUX_USERNAME: string;
-    readonly MINIFLUX_PASSWORD: string;
-    readonly MINIFLUX_URL: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
