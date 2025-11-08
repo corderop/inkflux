7de935b32a493c840f1a2c8dd3098d6be992aadf
Pablo Cordero <pablocorderoromero@gmail.com>
2025-11-08 12:25:08 +0100

    refactor: improve the middleware system and add JSDocs Working in: #9

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,17 @@
 import { defineMiddleware } from "astro:middleware";
-import { MINIFLUX_API_TOKEN_COOKIE_NAME, MINIFLUX_SERVER_URL_COOKIE_NAME } from "./config";
+import { MINIFLUX_API_TOKEN_COOKIE_NAME, MINIFLUX_SERVER_URL_COOKIE_NAME } from "../config";
 
-export const PUBLIC_PATHS = [
+const PUBLIC_PATHS = [
     '/login',
     '/api/login',
 ]
 
-export const onRequest = defineMiddleware(({ cookies, url, redirect }, next) => {
+/**
+ * Astro middleware for authentication.
+ * Checks if the user has a Miniflux API token and server URL in their cookies.
+ * If not, and the path is not public, it redirects them to the login page.
+ */
+export default defineMiddleware(({ cookies, url, redirect }, next) => {
     if (PUBLIC_PATHS.includes(url.pathname)) {
         return next();
     }
