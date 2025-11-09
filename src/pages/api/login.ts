c63638e020cf44da3e668d0b7dedfeab2af74bf2
Pablo Cordero <pablocorderoromero@gmail.com>
2025-11-09 17:00:05 +0100

    feat: clean URL introduced by the user to match the proper format Closes #20

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,6 +2,20 @@ import type { APIRoute } from 'astro';
 import AstroCookiesManager from '../../lib/astro-cookies-manager';
 import { MinifluxClient } from '../../lib/miniflux-client';
 
+function cleanUrl(url: string): string {
+    let cleanedUrl = url;
+
+    if (!url.includes('://')) {
+        cleanedUrl = `https://${cleanedUrl}`
+    }
+
+    if (url.slice(-1) === '/') {
+        cleanedUrl = cleanedUrl.slice(0, -1)
+    }
+
+    return cleanedUrl;
+}
+
 export const POST: APIRoute = async ({ request, redirect, cookies }) => {
     const formData = await request.formData();
     const url = formData.get('server');
@@ -11,7 +25,8 @@ export const POST: APIRoute = async ({ request, redirect, cookies }) => {
         return new Response('Form data is missing', { status: 400 });
     }
 
-    const minifluxClient = new MinifluxClient(url, apiToken);
+    const cleanedUrl = cleanUrl(url)
+    const minifluxClient = new MinifluxClient(cleanedUrl, apiToken);
     try {
         await minifluxClient.getCurrentUser();
     } catch {
@@ -20,7 +35,7 @@ export const POST: APIRoute = async ({ request, redirect, cookies }) => {
     }
 
     const cookieManager = new AstroCookiesManager(cookies)
-    cookieManager.setMinifluxSessionCookies({ url, apiToken })
+    cookieManager.setMinifluxSessionCookies({ url: cleanedUrl, apiToken })
 
     return redirect('/articles/0');
 };
